cc44fa9ce63b544b43ac283a50152be7dd0afb45
ratheeshsasidharan <mailtoratheesh.s@gmail.com>
2018-06-01 23:35:20 +1000

    spring security

--- src/main/java/com/mycart/MvcConfig.java
+++ src/main/java/com/mycart/MvcConfig.java
@@ -1,19 +1,17 @@
 package com.mycart;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
-public class MVCConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer {
 
-    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
+
 }
